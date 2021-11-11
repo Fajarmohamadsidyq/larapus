c5aabfc8a32f911cd379388a76c3abbd4df8413a
PriyadiSentosa <priyadisentosa825@gmail.com>
2021-11-11 11:04:07 +0700

    hallo

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,10 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function () {
-    Route::get('/', function (){
+    Route::get('/', function () {
         return view('admin');
     });
 });
